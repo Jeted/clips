ddee56617e01d3699a05230d70330b348da43bfd
Jeted <jeted@pm.me>
2021-09-10 17:56:28 +0200

    Rename header component to navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
   import { clips } from './misc/store';
   import Form from './components/Form.svelte';
-  import Header from './components/Header.svelte';
+  import Navbar from './components/Navbar.svelte';
   import Results from './components/Results.svelte';
 </script>
 
 <div class="container">
-  <Header />
+  <Navbar />
   <Form />
   {#if $clips.length}
     <Results />
