6c09caa1a7503cf17f182ae786c938c584de88ab
Jeted <jeted@pm.me>
2021-09-11 18:55:06 +0200

    Added gh-pages deploy script

--- src/config.sample.ts
+++ src/config.sample.ts
@@ -1,4 +1,16 @@
-export default {
+const config = {
   CLIENT_ID: '5lwlf9w9oltmzn9oquqgambkdbjr5d',
-  BASE_URL: 'http://localhost:5000/',
+
+  REMOTE_URL: 'https://jeted.github.io/',
+  LOCAL_URL: 'http://localhost:5000/',
+  REPO: 'clips',
+
+  get BASE_URL() {
+    if (window.location.href.includes(config.REMOTE_URL)) {
+      return config.REMOTE_URL + config.REPO;
+    }
+    return config.LOCAL_URL + config.REPO;
+  },
 };
+
+export default config;
